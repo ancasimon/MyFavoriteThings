7a734a5b1311a9d262305a777fc2fed002930f35
Anca Simon <ancarsimon@gmail.com>
2020-08-04 16:38:12 -0500

    Add project files.

---
+++ MyFavoriteThings/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFavoriteThings
+{
+    class Book
+    {
+        //properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string PubYear { get; set; }
+        public string Location { get; set; }
+
+        //fields
+        private bool _wasBookClubPick;
+
+        //constructor
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            Console.WriteLine($"{title} got instantiated!!");
+        }
+
+        //methods
+        public void Published()
+        {
+            if(PubYear != "")
+            {
+                Console.WriteLine($"{Title} was published in {PubYear}.");
+            }
+            else
+            {
+                Console.WriteLine($"We don't know when {Title} was published.");
+            }
+        }
+
+        public void BookClubRead(bool _wasBookClubPick)
+        {
+            if (_wasBookClubPick == true)
+            {
+                Console.WriteLine($"We did read {Title} in book club!");
+
+            }
+            else
+            {
+                Console.WriteLine($"We have not yet read {Title} in book club!");
+
+            }
+        }
+    }
+}
---
+++ MyFavoriteThings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFavoriteThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book1 = new Book("Beautiful Ruins", "Jess Walter");
+            //{
+            //    Title = "Beautiful Ruins",
+            //    Author = "Jess Walter",
+            //PubYear = "2012",
+            //Location = "Hollywood, CA; Florence, Italy"
+            //};
+            var book2 = new Book("Gone Girl", "Gillian Flynn");
+            //{
+            //    Title = "Gone Girl", 
+            //    Author = "Gillian Flynn",
+            //    //PubYear = "2012",
+            //    //Location = "Missouri"
+            //};
+
+            book1.BookClubRead(true);
+            book2.BookClubRead(false);
+        }
+    }
+}
